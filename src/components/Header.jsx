7d99496dd1a35733e0c7eda821d103f0d272c096
TeddyFer06 <72159786+TeddyFer06@users.noreply.github.com>
2023-08-07 11:41:10 -0500

    Add and configure the first classes of TailwindCSS

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <h1 className="font-black text-5xl text-center mx-20">
+            Seguimiento de Pacientes {''}
+            <span>Veterinaría</span>
+        </h1>
+    )
+}
+
+export default Header
